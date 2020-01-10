545a73e7c35e32046e956326e91e552a5f81a20c
Leandro.Carolino <carolino.leandro@gmail.com>
2020-01-11 00:05:06 +0100

    getting data from db

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+    // Load Config
+    require_once 'config/config.php';
+
+
+
+    // Autoload Core libraries
+    spl_autoload_register(function($className){
+        require_once 'libraries/' . $className . '.php';
+    });
---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '123456');
+    define('DB_NAME', 'mymvc');
+    // App root
+    define('APPROOT',dirname(dirname( __FILE__)));
+    // URL root
+    define('URLROOT', 'http://localhost/mymvc');
+    // Site name
+    define('SITENAME', 'mymvc');
